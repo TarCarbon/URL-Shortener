c9389c854d40fbfda09a8ea4351e5084ffebdf24
TarCarbon <126554519+TarCarbon@users.noreply.github.com>
2024-01-14 14:00:51 +0100

    Update V1__create_tables.sql

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS user (
-							                  user_name VARCHAR(45) CHECK (LENGTH(user_name) >=2) NOT NULL,
-								                email VARCHAR(45) PRIMARY KEY NOT NULL,
-							                  user_password VARCHAR(50) NOT NULL
-								                );
+				user_name VARCHAR(45) CHECK (LENGTH(user_name) >=2) NOT NULL,
+				email VARCHAR(45) PRIMARY KEY NOT NULL,
+				user_password VARCHAR(50) NOT NULL
+				);
 							
 CREATE TABLE IF NOT EXISTS link (
                                 short_link      VARCHAR(8) PRIMARY KEY    NOT NULL,
                                 user_id         VARCHAR(45)               NOT NULL,
                                 long_link       VARCHAR(500)              NOT NULL,
                                 data_create     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                active_status   INT   DEFAULT 0,
+                                active_status   TINYINT   DEFAULT 1,
                                 count_of_clicks INT       DEFAULT 0,
                                 FOREIGN KEY (user_id) REFERENCES user(email)
                                 );
