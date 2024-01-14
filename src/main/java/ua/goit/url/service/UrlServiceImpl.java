48627bbbda93a2aa9343a76f011916d2964efd94
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-01-15 00:02:18 +0200

    validate url

--- src/main/java/ua/goit/url/service/UrlServiceImpl.java
+++ src/main/java/ua/goit/url/service/UrlServiceImpl.java
@@ -1,4 +1,23 @@
 package ua.goit.url.service;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class UrlServiceImpl implements UrlService{
+
+    //before passing to the method, check and if necessary add to the originalUrl https://
+    private boolean isUrlAccessible(String originalUrl) {
+        int responseCode;
+
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(originalUrl).openConnection();
+            connection.setRequestMethod("HEAD");
+            responseCode = connection.getResponseCode();
+            connection.disconnect();
+        } catch (IOException e) {
+            responseCode = HttpURLConnection.HTTP_BAD_REQUEST;
+        }
+        return responseCode >= 200 && responseCode < 300;
+    }
 }
