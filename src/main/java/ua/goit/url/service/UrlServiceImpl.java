53663e3caa24b9076fdef61121944d40048e4fdf
Yurii Mezentsev <122153959+ymezentsev@users.noreply.github.com>
2024-01-15 17:08:54 +0200

    Correct UrlServiceImpl.java

--- src/main/java/ua/goit/url/service/UrlServiceImpl.java
+++ src/main/java/ua/goit/url/service/UrlServiceImpl.java
@@ -4,7 +4,19 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+@Service
+@RequiredArgsConstructor
 public class UrlServiceImpl implements UrlService{
+
+    private final UrlRepository repository;
+    
+    public List<UrlEntity> getAll() {
+        return repository.findAll();
+    }
+
+    public boolean isLinkUnique(String link) {
+        return getAll().stream().noneMatch(urlEntity -> urlEntity.getShortUrl().equals(link));
+    }
 
     //before passing to the method, check and if necessary add to the originalUrl https://
     private boolean isUrlAccessible(String originalUrl) {
