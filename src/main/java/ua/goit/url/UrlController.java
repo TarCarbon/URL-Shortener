fada3d2f2714f2f5e08af4b8e08479e3ee027eb1
DmytroAbramiuk <128092807+DmytroAbramiuk@users.noreply.github.com>
2024-01-15 18:37:18 +0200

    deleted mapper from url-controller

--- src/main/java/ua/goit/url/UrlController.java
+++ src/main/java/ua/goit/url/UrlController.java
@@ -2,13 +2,8 @@ package ua.goit.url;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ua.goit.url.dto.UrlDto;
-import ua.goit.url.mapper.UrlMapper;
-import ua.goit.url.request.CreateUrlRequest;
-import ua.goit.url.request.UpdateUrlRequest;
 import ua.goit.url.service.UrlService;
 
 import java.util.List;
@@ -18,8 +13,6 @@ import java.util.List;
 public class UrlController {
     @Autowired
     private UrlService urlService;
-    @Autowired
-    UrlMapper urlMapper;
 
     @GetMapping("/list")
     public List<UrlDto> urlList() {
