e4239be3df1678cd3c4641d101e3db80d6fdf892
Kostyantin <angrybobr@gmail.com>
2024-01-19 16:02:25 +0200

    tests for UrlRestController using RestAssured

--- src/test/resources/db/migration/V2__populate.sql
+++ src/test/resources/db/migration/V2__populate.sql
@@ -1,5 +1,10 @@
 INSERT INTO users (username, password, role)
-VALUES ('testuser', 'password', 'USER');
+VALUES  ('testadmin', 'qwerTy12', 'ADMIN'),
+		('testuser1', 'qwerTy12', 'USER'),
+		('testuser2', 'qwerTy12', 'USER');
 
-INSERT INTO urls (short_url, url, description, created_date, expiration_date, visit_count)
-VALUES ('testurl', 'https://some_long_named_portal.com/', 'for test only', '2024-01-18 12:34:56','2024-02-18 12:34:56', 1);
+INSERT INTO urls (short_url, url, description, user_id, created_date, expiration_date, visit_count)
+VALUES  ('testurl1', 'https://some_long_named_portal.com/', 'for test only', 1, '2024-01-18 12:34:56','2024-02-18 12:34:56', 1),
+		('testurl2', 'https://some_long_named_portal.com/', 'for test only', 1, '2024-01-18 12:34:56','2024-02-18 12:34:56', 1),
+		('testurl3', 'https://some_long_named_portal.com/', 'for test only', 2, '2024-01-18 12:34:56','2024-02-18 12:34:56', 1),
+		('testurl4', 'https://some_long_named_portal.com/', 'for test only', 3, '2024-01-18 12:34:56','2024-02-18 12:34:56', 1);
